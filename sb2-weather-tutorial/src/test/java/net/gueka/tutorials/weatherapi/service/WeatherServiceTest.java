7cb551e290bf7b7045e82d2ee83b8b8d3a697957
Gueka <gueka@pop-os.localdomain>
2019-08-09 13:12:54 -0400

    Added parametrized test

--- sb2-weather-tutorial/src/test/java/net/gueka/tutorials/weatherapi/service/WeatherServiceTest.java
+++ sb2-weather-tutorial/src/test/java/net/gueka/tutorials/weatherapi/service/WeatherServiceTest.java
@@ -2,12 +2,12 @@ package net.gueka.tutorials.weatherapi.service;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +15,6 @@ import org.springframework.web.client.RestTemplate;
 
 import net.gueka.tutorials.weatherapi.model.Weather;
 
-@ExtendWith(MockitoExtension.class)
 @SpringBootTest
 public class WeatherServiceTest {
 
@@ -25,16 +24,18 @@ public class WeatherServiceTest {
 	@InjectMocks
 	WeatherService service = new WeatherService();
 
-	@Test
-    public void getWeather() throws Exception {
+	@DisplayName("Should mock cp value and get the test weather everytime.")
+	@ParameterizedTest
+    @ValueSource(strings = { "000", "12345", "a", "1234567890987654321" })
+    public void getWeather(String cp) throws Exception {
 		Weather weather = Weather.builder().name("test").build();
         Mockito
           .when(template.getForEntity(
-			String.format(WeatherService.WEATHER_API_URL, "000", "us", "null"),
+			String.format(WeatherService.WEATHER_API_URL, cp, "", "null"),
 			Weather.class))
           .thenReturn(new ResponseEntity<Weather>(weather, HttpStatus.OK));
 
-		Weather response = service.getByZip("000", "us");
+		Weather response = service.getByZip(cp, "");
 
 		assertTrue(response.getName() == "test", "Has to return at leas a message");
     }
